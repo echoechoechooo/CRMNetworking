a2264baf23d2726b4c2c86551fa0fcb81a07ddd3
learn <learn@LEARN-13.local>
2019-12-03 13:03:21 -0800

    working on linkedin api request

--- networking_crm/app/controllers/users/omniauth_callbacks_controller.rb
+++ networking_crm/app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def linkedin
         # You need to implement the method below in your model (e.g. app/models/user.rb)
         @user = User.from_omniauth(request.env["omniauth.auth"])
-
+        p params[:code]
         if @user.persisted?
           sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
           set_flash_message(:notice, :success, kind: "LinkedIn") if is_navigational_format?
--- networking_crm/config/routes.rb
+++ networking_crm/config/routes.rb
@@ -1,4 +1,3 @@
-
 Rails.application.routes.draw do
     get '/auth/:provider/callback', to: 'sessions#create'
     devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
