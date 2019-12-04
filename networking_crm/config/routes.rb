a81b9e5561700beba7cd9767bbcda684d494ded9
Ubuntu <ubuntu@ip-172-31-25-134.us-east-2.compute.internal>
2019-12-04 19:47:56 +0000

    updateded users and todos to seed, deleted todos helper file

--- networking_crm/config/routes.rb
+++ networking_crm/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :todos
     get '/auth/:provider/callback', to: 'sessions#create'
     devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
     get '*path', to: 'pages#index', constraints: ->(request){ request.format.html? }
     root to: 'pages#index'
+    resources :todos
 end
