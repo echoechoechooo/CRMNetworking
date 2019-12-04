a73e5a6c54d437c10799c3a96e565c9ad359faf2
Ubuntu <ubuntu@ip-172-31-22-194.us-east-2.compute.internal>
2019-12-04 18:35:29 +0000

    completed seed data

--- networking_crm/db/seeds.rb
+++ networking_crm/db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.where(email:"joe@yahoo.com").first_or_create do |user|
+    user.update ({password:"Password"})
+end
+
+todo_data = [
+    {
+        due_date: nil,
+        is_done: false,
+        title: "Test",
+        description:"Testing the Seed Data"
+    },
+    {
+        due_date: nil,
+        is_done: false,
+        title: "Test2",
+        description:"2Testing the Seed Data"
+    },
+    ]
+    
+todo_data.each do |attributes|
+    user.todos.where(title:attributes[:title]).first_or_create do |todo|
+        todo.update(attributes)
+    end
+end
+    
+    
+    
+    
+    
+    
