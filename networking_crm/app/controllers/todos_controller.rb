16e9e371d9f66b14e927bd5022f12619d7001997
Ubuntu <ubuntu@ip-172-31-22-194.us-east-2.compute.internal>
2019-12-04 21:27:02 +0000

    did todos controller haven't validated working yet

--- networking_crm/app/controllers/todos_controller.rb
+++ networking_crm/app/controllers/todos_controller.rb
@@ -1,2 +1,44 @@
 class TodosController < ApplicationController
+    before_action :authenticate_user!, only: [:show, :create, :destroy, :edit, :update]
+    
+    def index
+        @todos = Todo.all
+    end
+    
+    def show
+    end
+    
+    def new
+        @todo = Todo.new
+    end
+    
+    def edit
+        @todo = current_user.todos.find params [:id]
+        render json: todo
+    end
+    
+    def create
+        @todo = current_user.todos.new(todo_params)
+        render json: todo, status:201
+    end
+    
+    def update
+        @todo = current_user.todos.find params [:id]
+        todo.update_attributes(todo_params)
+        render json: todo, status: 201
+    end
+    
+    def destroy
+        @todo = current_user.todos.find params[:id]
+        if todo.destroy
+            render json: todo
+        else
+            render json: {error: 'could not delete'}, status: 400
+        end
+    end  
+    
+    private
+    def todo_params
+        params.require(:todo).permit(:due_date, :is_done, :title, :description)
+    end
 end
