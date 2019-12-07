6c390a6ea49930bf5ea329f994dbc84b9ed9145a
learn <learn@LEARN-13.local>
2019-12-06 16:50:18 -0800

    contacts data shows up on contacts widget now. added create, edit, and delete contacts functionality.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,35 +1,43 @@
 class ContactsController < ApplicationController
     before_action :authenticate_user!, only: [:show, :create, :destroy, :update]
-    
+
     def index
-        @contacts = contact.all
+        @contacts = Contact.all
+        render json: @contacts
     end
-    
-    def show
-    end
-    
-    def create
-        @contact = current_user.contacts.new(contact_params)
-        render json: contact, status:201
-    end
-    
-    def update
-        @contact = current_user.contacts.find params [:id]
-        contact.update_attributes(contact_params)
+
+    def edit
+        contact = current_user.contacts.find params[:id]
         render json: contact, status: 201
     end
-    
+
+    def show
+        contact = Contact.find params[:id]
+        render json: contact
+    end
+
+    def create
+        contact = current_user.contacts.create contact_params
+        render json: contact, status:201
+    end
+
+    def update
+        @contact = current_user.contacts.find params[:id]
+        @contact.update_attributes(contact_params)
+        render json: @contact, status: 201
+    end
+
     def destroy
         @contact = current_user.contacts.find params[:id]
-        if contact.destroy
-            render json: contact
+        if @contact.destroy
+            render json: @contact
         else
             render json: {error: 'could not delete'}, status: 400
         end
-    end  
-    
+    end
+
     private
     def contact_params
-        params.require(:contact).permit(:first_name, :last_name, :location, :industry, :email_address, :phone_number, :notes)
+        params.require(:contact).permit(:first_name, :last_name)
     end
 end
--- app/javascript/components/pages/Dashboard.js
+++ app/javascript/components/pages/Dashboard.js
@@ -1,68 +1,87 @@
 import React from "react"
 import Sidebar from "react-sidebar";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from "react-router-dom";
+import NewContact from "./NewContact";
 
+
+import addButton from "../../images/plusButton.png"
 
 export default class Dashboard extends React.Component {
-  render () {
-    return (
-        <div>
-        <Sidebar
-        children={<div></div>}
-        sidebar={
-            <table className="table table-hover" style = {{width: "15vw"}}>
-              <thead>
-                <tr>
-                  <th scope="col" style = {{fontSize: "30px", padding: "10px 0px 10px 0px"}}>Contacts</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr onClick = {this.clickIt} className="table-active">
-                    <td style={{padding: "0px 0px 0px 0px"}}>
-                        <h3 style={{fontSize: "20px", padding: "0px 0px 0px 10px"}}>John Doe</h3>
-                        <h3 style={{fontSize: "12px", padding: "0px 0px 0px 10px"}}>Connect with John!</h3>
-                    </td>
-                </tr>
-                <tr onClick = {this.clickIt} className="table-active">
-                    <td style={{padding: "0px 0px 0px 0px"}}>
-                        <h3 style={{fontSize: "20px", padding: "0px 0px 0px 10px"}}>Jane Doe</h3>
-                        <h3 style={{fontSize: "12px", padding: "0px 0px 0px 10px"}}>Connect with Jane!</h3>
-                    </td>
-                </tr>
-                <tr onClick = {this.clickIt}>
-                    <th scope="row">Default</th>
-                </tr>
-                <tr className="table-primary">
-                  <th scope="row">Primary</th>
-                </tr>
-                <tr className="table-secondary">
-                  <th scope="row">Secondary</th>
-                </tr>
-                <tr className="table-success">
-                  <th scope="row">Success</th>
-                </tr>
-                <tr className="table-danger">
-                  <th scope="row">Danger</th>
-                </tr>
-                <tr className="table-warning">
-                  <th scope="row">Warning</th>
-                </tr>
-                <tr className="table-info">
-                  <th scope="row">Info</th>
-                </tr>
-                <tr className="table-light">
-                  <th scope="row">Light</th>
-                </tr>
-                <tr className="table-dark">
-                  <th scope="row">Dark</th>
-                </tr>
-              </tbody>
-            </table> }
-        docked = {true}
-        transitions = {false}
-        styles={{ sidebar: { top: "90px", background: "white"}, overlay: { backgroundColor:'clear', zIndex: -10 } }}
-        >
-        </Sidebar>
-        </div>
-    );
-  }
+
+    openContact = (id) => {
+        console.log(id)
+    }
+
+    addContact = () => {
+
+    }
+
+    render () {
+        const{contacts} = this.props
+        let contactList =  contacts.map(contact => {
+            return (
+                <tr onClick = {() => this.openContact(contact.id)} className="table-active">
+                        <td style={{padding: "0px 0px 0px 0px"}}>
+                            <Link to = {`/contacts/${contact.id}`} style={{fontSize: "20px", padding: "0px 0px 0px 10px"}}>{contact.first_name} {contact.last_name}</Link>
+                            <h3 style={{fontSize: "12px", padding: "0px 0px 0px 10px"}}>Connect with {contact.first_name}!</h3>
+                        </td>
+                    </tr>)
+        })
+
+        return (
+
+            <Sidebar
+            children={<div></div>}
+            sidebar={
+                <table className="table table-hover" style = {{width: "15vw"}}>
+                  <thead>
+                    <tr>
+                      <th scope="col" style = {{fontSize: "24px", padding: "10px 0px 10px 0px"}}>Contacts<Link to = {"/newcontact"} style={{padding: "0px 0px 0px 10px", float: "right"}}><img src = {addButton} style = {{width: "24px", height: "24px", verticalAlign: "inherit", textAlign: "right"}} /></Link></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {contactList}
+                  </tbody>
+                </table> }
+            docked = {true}
+            transitions = {false}
+            styles={{ sidebar: { top: "90px", background: "white"}, overlay: { backgroundColor:'clear', zIndex: -10 } }}
+            >
+            </Sidebar>
+            
+        );
+    }
 }
+
+// <tr onClick = {this.clickIt}>
+//     <th scope="row">Default</th>
+// </tr>
+// <tr className="table-primary">
+//   <th scope="row">Primary</th>
+// </tr>
+// <tr className="table-secondary">
+//   <th scope="row">Secondary</th>
+// </tr>
+// <tr className="table-success">
+//   <th scope="row">Success</th>
+// </tr>
+// <tr className="table-danger">
+//   <th scope="row">Danger</th>
+// </tr>
+// <tr className="table-warning">
+//   <th scope="row">Warning</th>
+// </tr>
+// <tr className="table-info">
+//   <th scope="row">Info</th>
+// </tr>
+// <tr className="table-light">
+//   <th scope="row">Light</th>
+// </tr>
+// <tr className="table-dark">
+//   <th scope="row">Dark</th>
+// </tr>
