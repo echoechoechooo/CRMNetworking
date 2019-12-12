39182d8106168217d94ca09e4f50991c2ec65d53
Ubuntu <ubuntu@ip-172-31-21-237.us-east-2.compute.internal>
2019-12-12 22:55:49 +0000

    todos crud rspec test done

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -16,9 +16,47 @@ RSpec.describe "Todos", type: :request do
       sign_in user
       params = {
         title: 'Todo1',
-        description: "tests todo1"
+        description: "tests todo1",
+        due_date: nil,
+        is_done: false
       }
       expect {post(todos_path,params: {todo:params} )}.to change(Todo, :count).by(1)
     end
   end
+  
+  context 'PUT #update' do
+    it "updates a todo for a logged in user" do
+      user = User.create!(email: "Joe@yahoo.com", password: "123456")
+      sign_in user
+      
+      todo = Todo.create!(title:"lets change", user: user)
+      params = {
+        title: 'updated Todo1',
+        description: "tests todo1",
+        due_date: nil,
+        is_done: false
+      }
+      patch todo_path(todo.id), params: { todo: params }
+      todo.reload
+      params.keys.each do |key|
+        expect(todo.attributes[key.to_s]).to eq params[key]
+       end
+    end
+  end
+  
+  context 'DELETE #destroy' do
+    it "deletes a todo for a logged in user" do
+      user = User.create!(email: "Joe@yahoo.com", password: "123456")
+      sign_in user
+      
+      todo = Todo.create!(title:"lets change", user: user)
+
+      # delete todo_path(todo.id)
+      # todo.reload
+      
+      # expect(todo.attributes[key.to_s]).to eq params[key]
+      expect {delete(todo_path(todo.id))}.to change(Todo, :count).by(-1)
+    end
+  end
+  
 end
