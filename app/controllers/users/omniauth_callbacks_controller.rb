6864e8e6c9984f40feb094ab2fdbf96f0739cc24
Ubuntu <ubuntu@ip-172-31-41-111.us-east-2.compute.internal>
2019-12-15 20:21:34 +0000

    sort contacts alphabetically, cleaned up omniauth code a bit

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -34,10 +34,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         response_following = HTTParty.get(@user.following_url)
         response_following.each do |following| 
             contact = @user.contacts.where(github_id:following["id"]).first_or_create
-            # contact.update(login:attributes["login"], avatar_url:attributes["avatar_url"], url:attributes["url"])
             response_following = HTTParty.get(following["url"])
-            # p response_following
-            
             contact.update(login:response_following["login"], avatar_url:response_following["avatar_url"], url:response_following["url"])
             if contact["location"]==nil || contact["location"].length == 0
                 contact.update(location:response_following["location"])
@@ -48,15 +45,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
             if (contact["last_name"]==nil || contact["last_name"].length == 0) && (response_following["name"] != nil && response_following["name"].split(' ').length > 1)
                 contact.update(last_name:response_following["name"].split(' ').last)
             end
-            # p response_following["name"]
         end
-        # p contacts
-        # sorting contacts alphabetically
-        # sort_alpha = HTTParty.get(@user.following_url)
-        # if (contacts["first_name"])
-        # contacts.sort_by! { |contact| contact["first_name"].downcase}
-        # end
-        # contacts.order()
         
         if @user.persisted?
           sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
