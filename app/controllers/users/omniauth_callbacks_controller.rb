b08133281326a28fe56e85a00a91207918113814
Ubuntu <ubuntu@ip-172-31-23-254.us-east-2.compute.internal>
2019-12-17 22:31:23 +0000

    added Github class, reduced followers/following method to 1

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,9 +1,24 @@
 require 'httparty'
 
+class GithubContacts
+    def initialize(user)
+       @user = user
+    end
+   
+    def followers
+       HTTParty.get(@user.followers_url)
+    end
+   
+    def following
+       HTTParty.get(@user.following_url)
+    end
+end
+
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def github
         # Sign-in via Github & update user table
         @user = User.from_omniauth(request.env["omniauth.auth"])
+        @github_contacts = GithubContacts.new(@user)
         
         
         access_token = request.env['omniauth.auth']['credentials'].token
@@ -12,40 +27,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         
         @user.update(access_token: access_token, strategy: "github", followers_url: followers, following_url: following)
         
-        # get for followers_url to input into Contacts
-        contacts = Contact.all
-        response_follower = HTTParty.get(@user.followers_url)
-        response_follower.each do |follower|
-            contact = @user.contacts.where(github_id:follower["id"]).first_or_create
-            response_follower = HTTParty.get(follower["url"])
-            contact.update(login:response_follower["login"], avatar_url:response_follower["avatar_url"], url:response_follower["url"])
-            if contact["location"]==nil || contact["location"].length == 0
-                contact.update(location:response_follower["location"])
-            end
-            if (contact["first_name"]==nil || contact["first_name"].length == 0) && response_follower["name"] != nil
-                contact.update(first_name:response_follower["name"].split(' ').first)
-            end
-            if (contact["last_name"]==nil || contact["last_name"].length == 0) && (response_follower["name"] != nil && response_follower["name"].split(' ').length > 1)
-                contact.update(last_name:response_follower["name"].split(' ').last)
-            end
-        end
-        
-        # get following_url data from Github
-        response_following = HTTParty.get(@user.following_url)
-        response_following.each do |following| 
-            contact = @user.contacts.where(github_id:following["id"]).first_or_create
-            response_following = HTTParty.get(following["url"])
-            contact.update(login:response_following["login"], avatar_url:response_following["avatar_url"], url:response_following["url"])
-            if contact["location"]==nil || contact["location"].length == 0
-                contact.update(location:response_following["location"])
-            end
-            if (contact["first_name"]==nil || contact["first_name"].length == 0) && response_following["name"] != nil
-                contact.update(first_name:response_following["name"].split(' ').first)
-            end
-            if (contact["last_name"]==nil || contact["last_name"].length == 0) && (response_following["name"] != nil && response_following["name"].split(' ').length > 1)
-                contact.update(last_name:response_following["name"].split(' ').last)
-            end
-        end
+        update_contacts(@github_contacts.following)
+        update_contacts(@github_contacts.followers)
         
         if @user.persisted?
           sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
@@ -54,7 +37,26 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
           session["devise.github_data"] = request.env["omniauth.auth"].except("extra")
           redirect_to new_user_registration_url
         end
-        
+    end
+    
+    def update_contacts(contacts)
+        contacts.each do |github_contact| 
+            contact = @user.contacts.where(github_id:github_contact["id"]).first_or_create
+            response_contact = HTTParty.get(github_contact["url"])
+            contact.update(login:response_contact["login"], avatar_url:response_contact["avatar_url"], url:response_contact["url"])
+            
+            if contact["location"].blank? # contact["location"].blank? contact["location"].present? contact["location"].presence 
+                contact.update(location:response_contact["location"])
+            end
+            
+            if contact["first_name"].blank? && response_contact["name"].present?
+                contact.update(first_name:response_contact["name"].split(' ').first)
+            end
+            
+            if contact["last_name"].blank? && response_contact["name"].present? && response_contact["name"].split(' ').length > 1
+                contact.update(last_name:response_contact["name"].split(' ').last)
+            end
+        end
     end
 
     def failure
